c790f2c57bb6aec04166fd39ece1efbc68e28251
Carlo Marcello <carlo.marcello@gmail.com>
2018-09-05 21:44:37 -0300

    Adicionando comentários úteis no arquivo counter.js

--- beginning-nodejs/chapter7/cookiesession/counter.js
+++ beginning-nodejs/chapter7/cookiesession/counter.js
@@ -5,6 +5,11 @@ var app = express()
     .use(cookieSession({
         keys: ['my super secret sign key']
     }))
+    // You can pass additional options in the cookieSession function (in addition to keys). 
+    // To specify the cookie name that the session is stored against, you can pass in the 
+    // name option. By default, the cookie name will be express:sess.  
+    // A few other options for cookie are also supported such as maxage, path, 
+    // httpOnly (true by default), and signed (true by default).
     .use('/home', function (req, res) {
         if (req.session.views) {
             req.session.views++;
