89a2a09acf81fd73538201c8c3e507c924e3977a
Carlo Marcello <carlo.marcello@gmail.com>
2019-03-10 19:34:52 -0300

    Removendo sub projeto

--- securing-restful-apis-with-jwt-master/db.js
+++
@@ -1,7 +0,0 @@
-
-// Usando base de dados no https://mlab.com
-// Para conectar pelo mongo shell:
-// $ mongo ds163382.mlab.com:63382/teste -u <dbuser> -p <dbpassword>
-
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://carlo:carlodbteste15@ds163382.mlab.com:63382/teste', { useMongoClient: true });
